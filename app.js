8a824dfff7dc8015f70f0356ea28a82402178661
Eduardo <eduardo.moraes.ritter@gmail.com>
2019-08-16 15:58:18 -0300

    1.0.0
    primeiro comit do multiplayer em sockt.io

---
+++ app.js
@@ -0,0 +1,181 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+const server = require("http").createServer(app)
+const io = require("socket.io")(server)
+
+app.use(express.static(path.join(__dirname)))
+
+// app.use("/teste", (req, res)=>{
+//     res.render('index.html')
+// })
+
+function uuidv4() {
+  return 'xxxxxx4xxyxxx'.replace(/[xy]/g, function (c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+let sala = ""
+
+const tic_tac_toe = {
+  // ATTRIBUTES
+  board: ['', '', '', '', '', '', '', '', ''],
+  symbols: {
+    options: ['O', 'X'],
+    turn_index: 0,
+    change() {
+      this.turn_index = (this.turn_index === 0 ? 1 : 0);
+    }
+  },
+  container_element: null,
+  gameover: false,
+  gameover: false,
+  winning_sequences: [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ],
+  check_winning_sequences(symbol, board) {
+    for (i in this.winning_sequences) {
+      if (board[this.winning_sequences[i][0]] == symbol &&
+        board[this.winning_sequences[i][1]] == symbol &&
+        board[this.winning_sequences[i][2]] == symbol) {
+        console.log('winning sequences INDEX:' + i);
+        return i;
+      }
+    };
+    return -1;
+  },
+  stylize_winner_sequence(winner_sequence) {
+    winner_sequence.forEach((position) => {
+      this
+        .container_element
+        .querySelector(`div:nth-child(${position + 1})`)
+        .classList.add('winner');
+    });
+  }
+}
+
+io.on("connection", (socket, nome) => {
+
+  console.log("conection ID: ", socket.id);
+
+  socket.on('inicia', function (nome) {
+    //limpa salas
+    socket.leave(socket.room)
+    io.sockets.sockets[socket.id].leave(socket.room)
+
+    if (sala == "" || io.sockets.adapter.rooms[sala] == undefined || io.sockets.adapter.rooms[sala].length == 2) {
+      sala = uuidv4()
+      socket.join(sala);
+      socket.room = sala
+      socket.symbols = "O"
+      io.sockets.adapter.rooms[socket.room].vez = socket.id
+      socket.emit('bora', [])
+      console.log("nova sala")
+    } else {
+      socket.join(sala);
+      socket.room = sala
+      socket.symbols = "X"
+      io.sockets.in(socket.room).emit('bora', tic_tac_toe.board)
+      console.log("entrou na sala ", tic_tac_toe.board)
+    }
+    io.sockets.adapter.rooms[socket.room].board = Object.assign([], tic_tac_toe.board)
+    io.sockets.adapter.rooms[socket.room].gameover = false
+  })
+
+  socket.on('jogada', function (jogada) {
+
+    var allRooms = io.rooms;
+    var myRooms = socket.rooms;
+
+    console.log("jogada do ",  socket.symbols, " - ", socket.id)
+
+    if (socket.room &&
+      io.sockets.adapter.rooms &&
+      io.sockets.adapter.rooms[socket.room] &&
+      io.sockets.adapter.rooms[socket.room].vez == socket.id &&
+      io.sockets.adapter.rooms[socket.room].gameover == false
+      ) {
+
+      var lista_socket = Object.keys(io.sockets.adapter.rooms[socket.room].sockets)
+      lista_socket.splice(lista_socket.indexOf(socket.id), 1);
+      io.sockets.adapter.rooms[socket.room].vez = lista_socket[0]
+      io.sockets.adapter.rooms[socket.room].board[jogada] = socket.symbols
+      board = io.sockets.adapter.rooms[socket.room].board
+      io.sockets.in(socket.room).emit('jogada', board)
+
+      let winning_sequences_index = tic_tac_toe.check_winning_sequences(socket.symbols, board)
+
+      if (winning_sequences_index >= 0) {
+        //this.stylize_winner_sequence(tic_tac_toe.winning_sequences[winning_sequences_index]);
+        //sequencia do vencedor stylize_winner_sequence
+        let winner_sequence = tic_tac_toe.winning_sequences[winning_sequences_index]
+        io.sockets.in(socket.room).emit('winner', winner_sequence)
+        io.sockets.adapter.rooms[socket.room].gameover = true
+      }
+    }
+  })
+
+  socket.on("disconnect", function () {
+    console.log("Disconnect:", Object.keys(socket));
+
+    //finaliza jogada por desconecao
+    io.sockets.in(socket.room).emit('bora', [])
+
+    //io.emit("update", clients[client.id] + " has left the server.");
+    //delete clients[client.id];
+  });
+
+});
+
+server.listen(3000)
+
+  // if (numClients == 0){
+  //   socket.join(room);
+  //   socket.emit('created', room);
+  // } else if (numClients == 1) {
+  //   io.sockets.in(room).emit('join', room);
+  //   socket.join(room);
+  //   socket.emit('joined', room);
+  // } else { // max two clients
+  //   socket.emit('full', room);
+  // }
+
+  // sala = "123"
+  // salas.push({
+  //   nome: sala,
+  //   id: client.id
+  // })
+
+  // client.join("sala");
+  // client.on("aaa", a => console.log("---", a))
+
+  //client.to(client.id).emit('event', { sala: sala });
+  //client.to('minha sala').emit('aaa')
+
+  // client.join(client.id).on("aaa", a => console.log(">>>", a))
+
+  // client.on('join', room => {
+  //   client.join(room);
+  // });
+
+  // client.on("aaaaaa", function (name) {
+  //   console.log("Joined: " + name);
+  //   //clients[client.id] = name;
+  //   client.emit("update", "You have connected to the server.");
+  //   client.broadcast.emit("update", name + " has joined the server.")
+  // });
+
+  // client.on("send", function (msg) {
+  //   console.log("Message: " + msg);
+  //   //client.broadcast.emit("chat", clients[client.id], msg);
+  // });
---
+++ js/tic-tac-toe.js
@@ -0,0 +1,123 @@
+// TIC TAC TOE
+const tic_tac_toe = {
+
+    socket : io('http://192.168.15.113:3000')
+    
+    .on('connect', () => {
+        console.log("aqui", tic_tac_toe.socket.disconnected); // false
+    })
+    .on('bora', board => {
+        if(board.length == 0){
+            tic_tac_toe.container_element.innerHTML = "Aguarde o outro jogador entrar na sala"
+        }else{
+            tic_tac_toe.board = board
+            tic_tac_toe.draw();
+        }        
+    })
+    .on("jogada", function (board) {
+        console.log(">>>>" + board)
+        tic_tac_toe.board = board;
+        tic_tac_toe.draw();
+    })
+    .on("winner", function (winner_sequence) {
+        tic_tac_toe.stylize_winner_sequence(winner_sequence);
+    })
+    .on("winner2", function (call) {
+        call()
+    }),
+
+    // ATTRIBUTES
+    board: ['', '', '', '', '', '', '', '', ''],
+    symbols: {
+        options: ['O', 'X'],
+        turn_index: 0,
+        change() {
+            this.turn_index = (this.turn_index === 0 ? 1 : 0);
+        }
+    },
+    container_element: null,
+    gameover: false,
+    // winning_sequences: [
+    //     [0, 1, 2],
+    //     [3, 4, 5],
+    //     [6, 7, 8],
+    //     [0, 3, 6],
+    //     [1, 4, 7],
+    //     [2, 5, 8],
+    //     [0, 4, 8],
+    //     [2, 4, 6]
+    // ],
+
+    // FUNCTIONS
+    init(container) {
+        this.container_element = container;
+    },
+    start() {
+        //this.board.fill('');
+        //this.draw();
+        //this.gameover = false;
+        var nome = document.getElementById("nome").value
+        this.socket.emit('inicia', nome)
+    },
+    // restart() {
+    //     if (this.is_game_over() || this.gameover) {
+    //         this.start();
+    //         console.log('this game has been restarted!')
+    //     } else if (confirm('Are you sure you want to restart this game?')) {
+    //         this.start();
+    //         console.log('this game has been restarted!')
+    //     }
+    // },
+    draw() {
+        tic_tac_toe.container_element.innerHTML = tic_tac_toe.board.map((element, index) => `<div onclick="tic_tac_toe.make_play('${index}')"> ${element} </div>`).reduce((content, current) => content + current);
+    },
+    make_play(position) {
+        this.socket.emit('jogada', position)
+
+        // if (this.gameover || this.board[position] !== '') return false;
+
+        // const currentSymbol = this.symbols.options[this.symbols.turn_index];
+        // this.board[position] = currentSymbol;
+        // this.draw();
+
+        // const winning_sequences_index = this.check_winning_sequences(currentSymbol);
+        // if (this.is_game_over()) {
+        //     this.game_is_over();
+        // }
+        // if (winning_sequences_index >= 0) {
+        //     this.game_is_over();
+        //     this.stylize_winner_sequence(this.winning_sequences[winning_sequences_index]);
+        // } else {
+        //     this.symbols.change();
+        // }
+        // return true;
+    },
+    stylize_winner_sequence(winner_sequence) {
+        winner_sequence.forEach((position) => {
+            this
+                .container_element
+                .querySelector(`div:nth-child(${position + 1})`)
+                .classList.add('winner');
+        });
+    },
+    // check_winning_sequences(symbol) {
+
+    //     for (i in this.winning_sequences) {
+    //         if (this.board[this.winning_sequences[i][0]] == symbol &&
+    //             this.board[this.winning_sequences[i][1]] == symbol &&
+    //             this.board[this.winning_sequences[i][2]] == symbol) {
+    //             console.log('winning sequences INDEX:' + i);
+    //             return i;
+    //         }
+    //     };
+    //     return -1;
+    // },
+    game_is_over() {
+        this.gameover = true;
+        console.log('GAME OVER');
+    },
+    is_game_over() {
+        return !this.board.includes('');
+    },    
+};
+
